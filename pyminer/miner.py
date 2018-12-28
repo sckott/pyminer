245593ff40b1d29a088cc08375b9ae9cb0c50566
Scott Chamberlain <myrmecocystus@gmail.com>
2018-12-28 14:48:05 -0800

    updates

--- pyminer/miner.py
+++ pyminer/miner.py
@@ -17,17 +17,62 @@ class Miner(object):
 
     from pyminer import Miner
     m = Miner()
-    # set your Crossref TDM key
-    Miner(tdm_key = "123456")
     # set a mailto address
     Miner(mailto = "foo@bar.com")
+    # set your Crossref TDM key
+    Miner(tdmkey = "123456")
+
+  **Authentication**
+
+    Authentication is applicable only when the publisher you want to get
+    fulltext from requires it. OA publishers shouldn't need it as you'd
+    expect, and there's some papers from typically closed publishers
+    that are open (a good way to check this is the license on the paper [
+    see `rcrossref`]). There's many publishers that don't share links at all,
+    so they are irrelevant here.
+
+    For publishers that required authentication, the Crossref TDM
+    program allows for a single token to authenticate across publishers
+    (to make it easier for text miners). The publishers involved with the
+    authentication scheme are really only Elsevier and Wiley - these two
+    publishers make up a lot of the papers out there and a lot of the
+    links on the Crossref API.
+
+    There's a how to guide for Crossref TDM at
+    <http://tdmsupport.crossref.org/researchers/>.
+    Get your Crossref TDM token by registering at
+    <https://apps.crossref.org/clickthrough/researchers>
+
+  **IP addresses**
+
+    If you don't know what IP addresses are, check out
+    <https://en.wikipedia.org/wiki/IP_address>. At least Elsevier and
+    I think Wiley also check your IP address in addition to requiring the
+    authentication token. Usually your're good if you're physically at the
+    institution that has access to the publishers content OR on a VPN
+    (i.e., pretending you're there).
+
+    If you forget about this, you'll get errors about not being authorized.
+    So check and make sure you're on a VPN if you're not physically
+    located at your institution.
+
+  **Fences**
+
+    There's yet another issue to worry about. At least with Elsevier, they
+    have a so-called "fence" - that is, even if an institution has access
+    to Elsevier content, Elsevier doesn't necessarily have the fence
+    turned off - if its not off, you can't get through - if it's off, you can.
+    If you have the right token and you are sure you're on the right
+    IP address, this could be the problem for your lack of access. If that happens
+    get in touch with me at <mailto:scott@ropensci.org> and i'll try to sort it out.
+
   
   |
   |
   |
   '''
-  def __init__(self, tdm_key = None, mailto = None):
-    self.tdm_key = tdm_key
+  def __init__(self, mailto = None, tdmkey = None):
+    self.tdm_key = tdmkey
     self.mailto = mailto
 
   def __repr__(self):
